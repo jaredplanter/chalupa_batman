a3396cab6570a846c8fe265d33a835717a102741
Jared Planter <jared.planter@activenetwork.com>
2013-09-21 12:57:32 -0700

    OMG here we go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChalupaBatman::Application.config.secret_key_base = 'a528575d0bbe21e2a380a45481f9116939c256d0ed167322d7f3757a6d6a2fe187f10584b903236e2034086eaaad6f60d2be1d2ab2690bb224dd0f10147eb957'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChalupaBatman::Application.config.session_store :cookie_store, key: '_chalupa_batman_session'
