640da3f66df00ed2c6cd4fb65afe8cce987bd145
Jared Planter <jared.planter@activenetwork.com>
2013-09-21 15:04:12 -0700

    added users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 ChalupaBatman::Application.routes.draw do
+  get "users/index"
+  get "users/new"
+  get "users/create"
+  get "users/edit"
+  get "users/destroy"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
