019999aaf6e6bf7612578eaefae7889ccd09bf25
cgoldman <catilin.goldman@activenetwork.com>
2013-09-22 00:10:28 -0700

    navbar BITCH

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,47 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.min
+
+$(document).ready(function()
+{
+	$('#searchMenuItem').click(function() {
+		window.location = '/events'
+		$('.dropdown-toggle').text('Search');
+	});
+
+	$('#profileMenuItem').click(function() {
+		window.location = '/user/1'
+		$('.dropdown-toggle').text('Profile');
+	});
+
+	$('#findSparkMenuItem').click(function() {
+		window.location = '/events'
+		$('.dropdown-toggle').text('Create Sparks');
+	});
+
+	$('#sparksMenuItem').click(function() {
+		window.location = '/events'
+		$('.dropdown-toggle').text('Sparks');
+	});
+
+	$('#requestsMenuItem').click(function() {
+		window.location = '/events'
+		$('.dropdown-toggle').text('Requests');
+	});
+
+	$('#menu').click(function() {
+		if (!$('menuItems').is(":visible")) {
+			$('#menuItems').show();
+		}
+		else {
+			$('#menuItems').hide();
+		}
+	});
+
+	$(document).bind('click', function(e) {
+		if($(e.target).closest('#menu').length === 0) {
+			$('#menuItems').hide();
+		}
+	});
+
+});
