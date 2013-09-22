395dd8e4b57c6f315cd29b6cca4489e8eb20f0d3
cgoldman <catilin.goldman@activenetwork.com>
2013-09-22 05:25:37 -0700

    spark lookup page

--- app/assets/javascripts/lookup.js
+++ app/assets/javascripts/lookup.js
@@ -1,12 +1,15 @@
 $(document).ready(function()
 {	
 	$('#trainAlone').click( function() {
-		var id = $(id).val();
-		window.location(/user/ + id + 1);
+		var id = $('#id').prop('innerText');
+		var newId = parseInt(id) + 1;
+		window.location = '/lookup/' + newId;
 	});
 
 	$('#sparkMyTraining').click( function() {
-		var id = $(id).val();
-		window.location(/user/ + id + 1);
-	});
+		var id = $('#id').prop('innerText');
+		var newId = parseInt(id) + 1;
+		window.location = '/lookup/' + newId;
+	})
 });
+
