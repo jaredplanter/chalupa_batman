72debe6511001690a765059bc07976203cf67668
Jared Planter <jared.planter@activenetwork.com>
2013-09-22 01:11:04 -0700

    fixing some shit i broke

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,4 +13,11 @@ class SearchController < ApplicationController
 
   def destroy
   end
+
+  def events
+    if params[:keyword].present?
+      @event = ACTV.search params[:keyword], { near: "San Diego, CA", start_date: "2013-09-22..", category: "event", exclude_children: true }
+    end
+    
+    @interests = Interest.all
 end
